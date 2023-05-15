79ccd009ad1c36fdad8b6a2ca90161ca89cbb8ae
Dan-McGrath <mcgrathd3254@yahoo.com>
2023-05-15 18:30:12 -0400

    Add Mock Data

---
+++ src/mockdata.js
@@ -0,0 +1,33 @@
+export const tracksArray = [
+    {
+        id: 1,
+        artist: 'Artist 1',
+        songName: 'Song 1',
+        duration: 'Duration'
+    },
+    {
+        id: 2,
+        artist: 'Artist 2',
+        songName: 'Song 2',
+        duration: 'Duration'
+    },
+    {
+        id: 3,
+        artist: 'Artist 3',
+        songName: 'Song 3',
+        duration: 'Duration'
+    },
+    {
+        id: 4,
+        artist: 'Artist 4',
+        songName: 'Song 4',
+        duration: 'Duration'
+    },
+    {
+        id: 5,
+        artist: 'Artist 5',
+        songName: 'Song 5',
+        duration: 'Duration'
+    }
+]
+
