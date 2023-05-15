5a3d6bcf460f7d2039271a0bd1ac89c9bb793bb4
Dan-McGrath <mcgrathd3254@yahoo.com>
2023-05-15 18:30:26 -0400

    Add components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import './App.scss'
+import Header from './components/Header'
+import { tracksArray } from './mockdata'
+import SearchResults from './components/SearchResults'
+import Playlist from './components/Playlist'
 
 function App() {
-
+  const [tracks, setTracks] = useState(tracksArray)
 
   return (
     <>
-      
+      <Header />
+      <main>
+        <SearchResults />
+        <Playlist />
+      </main>
     </>
   )
 }
