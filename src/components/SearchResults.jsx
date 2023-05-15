492b740908d2cdbe06474fe5b91996134ecbeb4f
Dan-McGrath <mcgrathd3254@yahoo.com>
2023-05-15 18:30:02 -0400

    Create SearchResults

---
+++ src/components/SearchResults.jsx
@@ -0,0 +1,15 @@
+import Tracklist from "./Tracklist"
+
+
+const SearchResults = () => {
+
+    return (
+        <div className="search-results">
+           <h2>Search Results</h2>
+           <Tracklist /> 
+        </div>
+        
+    )
+}
+
+export default SearchResults
